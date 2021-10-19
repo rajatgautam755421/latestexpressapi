c14da2b661aca7e2bd53c8c77371e8c97d718ca7
rajat755421 <rajatgautam832@gmail.com>
2021-10-19 14:34:26 +0545

    first commit

---
+++ Server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("./Config/Database");
+const Port = process.env.PORT || 3000;
+
+// Global Level Middlewares
+app.use(cors());
+app.use(express.json());
+
+//Importing Routes
+const contactRoute = require("./Routes/Contact.Route");
+
+//Configuire Routes
+app.use("/api/v1", contactRoute);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: "Welcome",
+  });
+});
+
+app.listen(Port, () => {
+  console.log("Server is up and running at 3000 🚀 ");
+});
